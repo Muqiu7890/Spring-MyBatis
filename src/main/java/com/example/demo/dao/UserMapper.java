2972194fa946561aa1ced2ab350e2ffb3d37b2f1
Muqiu7890 <1337651840@qq.com>
2019-04-23 15:52:20 +0800

    Add answer

--- src/main/java/com/example/demo/dao/UserMapper.java
+++ src/main/java/com/example/demo/dao/UserMapper.java
@@ -1,17 +1,25 @@
 package com.example.demo.dao;
 
+import com.example.demo.entity.User;
+import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserMapper {
-    // 1. 实现查询所有用户
+    @Select("select * from user")
+    List<User> findAll();
 
-    // 2. 根据用户id查询用户
+    @Select("select * from user where id=#{id}")
+    User findById(@Param("id") long bookId);
 
-    // 3. 添加新用户
+    @Delete("delete from user where id = #{id}")
+    void deleteById(@Param("id") long id);
 
-    // 4. 根据用户id删除用户
+    @Insert("insert into user(name) values(#{name})")
+    void insertUser(@Param("name") String name);
 
-    // 5. 根据用户id更新用户
-
+    @Update("update user set name = #{name} where id = #{id}")
+    void updateUser(@Param("name") String name,@Param("id") int id);
 }
